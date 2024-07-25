49c6109775a4d35005b119540e4c03aafc38ca49
Manu Goyal <manu.goyal2013@gmail.com>
2024-07-24 21:10:02 -0700

    Enable copilot by default

--- init.lua
+++ init.lua
@@ -78,9 +78,7 @@ require("lazy").setup({
   "junegunn/fzf",
   "junegunn/fzf.vim",
   "manugoyal/githubify",
-  {"github/copilot.vim",
-   cmd="Copilot disable",
-  },
+  "github/copilot.vim",
 })
 
 -- Fzf
